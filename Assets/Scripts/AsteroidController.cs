013c49d27ed1fd03b2590e020f21089ca67c25c2
Derek O'Kelly <derekokelly1997@gmail.com>
2020-04-18 16:16:35 +0100

    Implement basic asteroid spawning & rotation.

---
+++ Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour
+{
+    Vector3 euler;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        euler = transform.eulerAngles;
+        euler.z = Random.Range(1.0f, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles += euler;
+
+        MoveToCenter();
+    }
+
+    private void MoveToCenter()
+    {
+        Vector2 target = Vector2.zero; // center of the screen
+        Vector2 newPos = Vector2.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        transform.position = newPos;
+    }
+}
