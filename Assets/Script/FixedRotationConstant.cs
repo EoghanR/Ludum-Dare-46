1702dc73ef137af06170de1b7e6b7c1e19b4ecae
EoghanR <32027757+EoghanR@users.noreply.github.com>
2020-04-18 16:03:39 +0100

    ERC-1 - Added Basic Tile map and rotation scripts to planet and clouds.

---
+++ Assets/Script/FixedRotationConstant.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedRotationConstant : MonoBehaviour
+{
+
+    public long rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Will rotate "rotationSpeed" coords every second in the anti-clockwise direction.
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
