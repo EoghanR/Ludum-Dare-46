91ede0c5d08b949cbf2b76d47324e9f18825aad5
EoghanR <32027757+EoghanR@users.noreply.github.com>
2020-04-18 20:34:54 +0100

    ERC-2b - Added gun - shield to planet and have it firing bullets + integrate Derek's Asteroid changes

---
+++ Assets/Scripts/ShootProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
+    }
+}
