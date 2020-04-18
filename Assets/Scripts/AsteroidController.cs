44181eb7fee3085a98c34723783bad71d1b85749
Derek O'Kelly <derekokelly1997@gmail.com>
2020-04-18 16:48:49 +0100

    Choose random sprite on asteroid instantiation.

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AsteroidController : MonoBehaviour
-{
+{ 
+
+    public Sprite[] sprites;
+    int index;
+
     Vector3 euler;
 
     public float speed;
@@ -11,6 +15,9 @@ public class AsteroidController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        index = Random.Range(0, sprites.Length);
+        GetComponent<SpriteRenderer>().sprite = sprites[index];
+
         euler = transform.eulerAngles;
         euler.z = Random.Range(1.0f, 3.0f);
     }
