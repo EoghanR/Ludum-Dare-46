a857572c319d8328f1b802b85e530b6863c5852c
Derek O'Kelly <derekokelly1997@gmail.com>
2020-04-19 15:58:26 +0100

    Add basic state machine.

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,27 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameLogic : MonoBehaviour
 {
     AsteroidSpawner asteroidSpawner;
 
-    bool gameStarted = false;
+    private static bool introRan = false;
+
+    enum State { Intro, Running, Dead };
+
+    State state;
 
     void Start()
     {
         asteroidSpawner = GameObject.Find("World").GetComponent<AsteroidSpawner>();
         asteroidSpawner.enabled = false;
+        state = State.Intro;
     }
 
     void Update()
     {
+
+        CheckState();
+
         // replace with
         // click start button to start game
         if (Input.GetMouseButtonDown(0))
         {
-            gameStarted = true;
-            asteroidSpawner.enabled = true;
+            state = State.Running;
+            introRan = true;
+        }
+
+        // reload scene if R button is pressed
+        if (Input.GetKey("r"))
+        {
+            ReloadCurrentScene();
+        }
+    }
+
+    public void ReloadCurrentScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void CheckState()
+    {
+        switch (state)
+        {
+            case State.Intro:
+                if (introRan)
+                {
+                    state = State.Running;
+                }
+                break;
+            case State.Running:
+
+                // hide instructions & start game
+                GameObject.Find("Canvas").GetComponent<Canvas>().transform.GetChild(3).gameObject.SetActive(false);
+                GameObject.Find("Canvas").GetComponent<Canvas>().transform.GetChild(4).gameObject.SetActive(false);
+
+                asteroidSpawner.enabled = true;
+
+                break;
+            case State.Dead:
+                // reload current scene
+                ReloadCurrentScene();
+                break;
         }
     }
 }
